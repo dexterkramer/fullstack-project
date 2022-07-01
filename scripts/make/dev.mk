31041b25bb3de8f14e536c01c11ca1b1bdf7c029
Benoît Sauveton <bsauveton@gmail.com>
2022-07-01 15:40:30 +0200

    add submodule

---
+++ scripts/make/dev.mk
@@ -0,0 +1,24 @@
+.PHONY: status logs start stop clean
+
+status: ## Get status of containers
+	docker-compose ps
+
+logs: ## Get logs of containers
+	docker-compose logs --tail=0 --follow
+
+start:build ## Build and start docker containers
+	docker-compose up -d
+
+dev: 
+	docker-compose up
+
+destroy-all:
+	docker-compose stop
+	docker-compose down -v --remove-orphans
+	docker system prune -a --volumes -f
+
+stop: ## Stop docker containers
+	docker-compose stop
+
+clean:stop ## Stop docker containers, clean data and workspace
+	docker-compose down -v --remove-orphans
