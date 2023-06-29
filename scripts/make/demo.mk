afa7e4cde043501d8594115b6a200b4c93dc92ca
dexter <dexter@desktop-hh17ebh.home>
2023-06-29 06:40:21 +0200

    deploy in another folder

--- scripts/make/demo.mk
+++ scripts/make/demo.mk
@@ -14,9 +14,9 @@ importDemo:
 	make importKeycloakRealm filepath="/tmp/import/demo.json"
 
 moveDumps:
-	mkdir -p /projects/demo/datas/neo4j-docker/neo4j-runtime/import/
+	mkdir -p projects/demo/datas/neo4j-docker/neo4j-runtime/import/
 	cp demo/__dump/neo4j/demo.dump projects/demo/datas/neo4j-docker/neo4j-runtime/import/demo.dump
-	sudo cp demo/__dump/keycloak/demo.json projects/demoLkeycloak/realms/import/demo.json
+	sudo cp demo/__dump/keycloak/demo.json projects/demo/keycloak/realms/import/demo.json
 	sudo cp demo/__dump/keycloak/kafka.json projects/demo/keycloak/realms/import/kafka.json
 
 moveEnvs:
@@ -40,13 +40,21 @@ build-demo:
 	echo "Demo installed..."
 
 deployDemo:
-    rm -rf projects/demo
-    mkdir -p projects/demo/
-    mkdir -p projects/demo/apis/
-    mkdir -p projects/demo/clients/
-    mkdir -p projects/demo/datas/
-    cp -rf /keycloak/ projects/demo/keycloak/
-    cp -rf /kafka/ projects/demo/kafka/
-    cp docker-compose.yml projects/demo/docker-compose.yml
-    cp docker-compose-kafka.yml projects/demo/docker-compose-kafka.yml
-    make getRepos
+	rm -rf projects/demo/
+	mkdir -p projects/demo/
+	mkdir -p projects/demo/apis/
+	mkdir -p projects/demo/clients/
+	mkdir -p projects/demo/datas/
+	cp -R keycloak/ projects/demo/keycloak/
+	cp -R kafka/ projects/demo/kafka/
+	cp docker-compose.yml projects/demo/docker-compose.yml
+	cp docker-compose-kafka.yml projects/demo/docker-compose-kafka.yml
+	make getRepos
+	make moveEnvs
+	make moveDumps
+	make localizeImage
+	make loadLocalizedImage
+	docker compose -p fullstack -f projects/demo/docker-compose.yml up -d
+	make localizeAfterNpm
+	make importDemo
+	docker compose -p fullstack -f projects/demo/docker-compose.yml -f projects/demo/docker-compose-kafka.yml up -d
