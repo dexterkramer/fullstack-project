80de9551c3c0da8d52fdb3e13c66b6b46e9f6a87
dexter <dexter@desktop-hh17ebh.home>
2023-08-11 06:00:36 +0200

    wip

--- scripts/make/demo.mk
+++ scripts/make/demo.mk
@@ -27,6 +27,46 @@ getRepos:
 	git clone git@github.com:dexterkramer/demo-frontend.git ./projects/demo/clients/demo-frontend
 	git clone git@github.com:dexterkramer/neo4j-docker.git ./projects/demo/datas/neo4j-docker
 
+localizeAfterNpm:
+	docker save my_api:latest > images/my_api.tar
+	docker save my_client:latest > images/my_client.tar
+
+localizeImage:
+	mkdir -p images
+	docker pull postgres
+	docker save postgres > images/postgres.tar
+	docker pull quay.io/keycloak/keycloak:19.0.1
+	docker save quay.io/keycloak/keycloak:19.0.1 > images/keycloak.19.0.1.tar
+	docker pull confluentinc/cp-zookeeper
+	docker save confluentinc/cp-zookeeper > images/cp-zookeeper.tar
+	docker pull confluentinc/cp-kafka
+	docker save confluentinc/cp-kafka > images/cp-kafka.tar
+	docker pull confluentinc/cp-schema-registry
+	docker save confluentinc/cp-schema-registry > images/cp-schema-registry.tar
+	docker pull confluentinc/cp-kafka-connect
+	docker save confluentinc/cp-kafka-connect > images/cp-kafka-connect.tar
+	docker pull confluentinc/cp-enterprise-control-center
+	docker save confluentinc/cp-enterprise-control-center > images/cp-enterprise-control-center.tar
+	docker pull neo4j:4.4.8-community
+	docker save neo4j:4.4.8-community > images/neo4j.4.4.8-community.tar
+	docker pull node:16.15.0-alpine
+	docker save node:16.15.0-alpine > images/node.16.15.0-alpine.tar
+
+# docker load < images/node.16.15.0-alpine.tar
+loadLocalizedImage: 
+	docker load < images/postgres.tar
+	docker load < images/keycloak.19.0.1.tar
+	docker load < images/cp-zookeeper.tar
+	docker load < images/cp-kafka.tar
+	docker load < images/cp-schema-registry.tar
+	docker load < images/cp-kafka-connect.tar
+	docker load < images/cp-enterprise-control-center.tar
+	docker load < images/neo4j.4.4.8-community.tar
+
+LoadLocalizedImageNpm: 
+	docker load < images/my_api.tar
+	docker load < images/my_client.tar
+
 deployDemo:
 	sudo rm -rf projects/demo/
 	mkdir -p projects/demo/
